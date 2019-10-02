0d3ccca6c0df41eac8a7d53b63e7517244d7e9f6
MrHorton15 <4stephenhorton@gmail.com>
2019-10-02 16:12:43 -0600

    updats to components

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 
+import PortfolioItem from "./portfolio-item";
+
 export default class PortfolioContainer extends Component {
   constructor() {
     super();
+
     console.log("Portfolio container has rendered");
+  }
+
+  portfolioItems() {
+    const data = ["Red", "Orange", "Yellow", "Green"];
+
+    return data.map(item => {
+      return <PortfolioItem title={item} url={"google.com"} />;
+    });
   }
 
   render() {
     return (
       <div>
         <h2>Portfolio items go here... Updated</h2>
+
+        {this.portfolioItems()}
       </div>
     );
   }

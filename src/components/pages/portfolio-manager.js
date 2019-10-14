cf530fa66d09567243f0e1b032e201833ca56854
MrHorton15 <4stephenhorton@gmail.com>
2019-10-14 13:53:35 -0600

    Added font awesome styling

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -3,30 +3,52 @@ import axios from "axios";
 
 import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 import PortfolioForm from "../portfolio/portfolio-form";
+import { prototype } from "stream";
 
 export default class PortfolioManager extends Component {
   constructor() {
     super();
 
     this.state = {
-      portfolioItems: []
+      portfolioItems: [],
+      portfolioToEdit: {}
     };
-
-    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
-      this
-    );
-    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
   }
 
-  handleSuccessfulFormSubmission(portfolioItem) {
+  handleEditClick = portfolioItem => {
+    this.setState({
+      portfolioToEdit: portfolioItem
+    });
+  };
+
+  handleDeleteClick = portfolioItem => {
+    axios
+      .delete(
+        `https://stephenhorton.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.setState({
+          portfolioItems: this.state.portfolioItems.filter(item => {
+            return item.id !== portfolioItem.id;
+          })
+        });
+        return response.data;
+      })
+      .catch(error => {
+        console.log("handleDeleteClick error", error);
+      });
+  };
+
+  handleSuccessfulFormSubmission = portfolioItem => {
     this.setState({
       portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
     });
-  }
+  };
 
-  handleFormSubmissionError(error) {
+  handleFormSubmissionError = error => {
     console.log("handleFormSubmissionError error", error);
-  }
+  };
 
   getPortfolioItems() {
     axios
@@ -61,7 +83,11 @@ export default class PortfolioManager extends Component {
         </div>
 
         <div className="right-column">
-          <PortfolioSidebarList data={this.state.portfolioItems} />
+          <PortfolioSidebarList
+            handleDeleteClick={this.handleDeleteClick}
+            data={this.state.portfolioItems}
+            handleEditClick={this.handleEditClick}
+          />
         </div>
       </div>
     );

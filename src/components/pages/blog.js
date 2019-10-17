a9d9e52ef7524d60277e028385a6b38f7b68ed90
MrHorton15 <4stephenhorton@gmail.com>
2019-10-16 18:21:23 -0600

    added infinite scroll

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
 
@@ -8,18 +9,49 @@ class Blog extends Component {
     super();
 
     this.state = {
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
+      isLoading: true
+    };
+
+    this.activateInfiniteScroll();
+  }
+
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+      //does not call the API if state isLoading is true or if we have hit max blog count
+      if (
+        this.state.isLoading ||
+        this.state.blogItems.length === this.state.totalCount
+      ) {
+        return;
+      }
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        this.getBlogItems();
+      }
     };
   }
 
   getBlogItems = () => {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
     axios
-      .get("https://stephenhorton.devcamp.space/portfolio/portfolio_blogs", {
-        withCredentials: true
-      })
+      .get(
+        `https://stephenhorton.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
+        {
+          withCredentials: true
+        }
+      )
       .then(response => {
         this.setState({
-          blogItems: response.data.portfolio_blogs
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+          totalCount: response.data.meta.total_records,
+          isLoading: false
         });
       })
       .catch(error => {
@@ -35,7 +67,17 @@ class Blog extends Component {
     const blogRecords = this.state.blogItems.map(blogItem => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
-    return <div>{blogRecords}</div>;
+
+    return (
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
+        {this.state.isLoading ? (
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 
